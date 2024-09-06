7799b8e2d7752fa8fa852e995175615f2260ab3d
Dominik Berner <dominik.berner@gmail.com>
2024-09-06 09:20:24 +0200

    Ignore commented out lines

--- docs/_posts/2024-09-03-huenerfuedle.md
+++ docs/_posts/2024-09-03-huenerfuedle.md
@@ -8,4 +8,4 @@ excerpt_separator: <!--more-->
 Echte Bodybuilder essen nur Pouletbrust ohne Haut. Wer eher auf Vollverwertung steht, brät sich auch das [Hüenerfüdle](https://s.geo.admin.ch/kzwslm7s4iq2)
 
 Wer sich diesen Flurnamen ausgedacht hat, lässt sich beim besten Willen nicht mehr eruieren. Ob es dereinst mal wilde Hühner in diesem hügeligen Gelände gab, und ob diese durch besonders prachtvolle Hinterteile auffielen, bleibt ein Rätsel.
-<!--Tore>
+<!--more -->
--- spell_checker.py
+++ spell_checker.py
@@ -9,15 +9,10 @@ def correct_text(text, tool):
     return corrected_text
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python spell_checker.py <markdown_file>")
-        sys.exit(1)
-
-    file_path = sys.argv[1]
+def correct_file(filename):
     tool = language_tool_python.LanguageTool('de-CH')
 
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
 
     # Split content into lines
@@ -36,8 +31,9 @@ def main():
             corrected_lines.append(line)
             continue
 
-        # Exclude hyperlinks and hyperlink labels
-        if re.match(r'\[.*\]\(.*\)', line):
+        # Exclude hyperlinks, hyperlink labels, and commented out lines
+        if re.match(r'\[.*\]\(.*\)', line) or re.match(r'<!--.*-->', line):
+            print(f"Skipping line: {line}")
             corrected_lines.append(line)
             continue
 
@@ -49,8 +45,17 @@ def main():
     corrected_content = '\n'.join(corrected_lines)
 
     # Write corrected content back to the file
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(filename, 'w', encoding='utf-8') as file:
         file.write(corrected_content)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python spell_checker.py <markdown_file>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    correct_file(file_path)
 
 
 if __name__ == "__main__":
